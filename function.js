de6c066f334fc5222b3d33c5c574031dfb018e66
Nick Jackson <nick@dxw.com>
2020-05-10 16:02:47 +0100

    2020-05-10

--- function.js
+++ function.js
@@ -10,8 +10,50 @@ exports.handler = function (context, event, callback) {
     .then(function (response) {
       data = JSON.parse(response.body);
 
-      twiml.say("Thanks for calling " + data.church_name + ".");
+      const services = data.services.slice(0, 10); // Get the latest 9 services
 
+      if (
+        data === undefined ||
+        services === undefined ||
+        services.length === 0
+      ) {
+        twiml.say(
+          "Sorry , we ran into a problem fetching the latest services."
+        );
+        callback(null, twiml);
+        twiml.hangup();
+      }
+
+      // No digit pressed? Spit out the root menu.
+      if (event.Digits === undefined) {
+        const service_menu = services
+          .map(
+            (service, index) => "Press " + (index + 1) + " for " + service.title
+          )
+          .join(",");
+
+        twiml
+          .gather({ numDigits: 1 })
+          .say("Thanks for calling " + data.church_name + ". " + service_menu);
+      } else {
+        // Key pressed, do the thing
+
+        const service_id = event.Digits - 1;
+        const service = services[service_id];
+
+        if (service === undefined) {
+          twiml.say("Sorry, we ran into a problem fetching the service.");
+          callback(null, twiml);
+          twiml.hangup();
+        }
+
+        twiml.say(
+          "Please wait whilst we fetch the service. It may take a few seconds to connect you."
+        );
+        let service_url = das_root_url + "audio/" + service.file;
+        twiml.play(service_url);
+        twiml.hangup();
+      }
       callback(null, twiml);
     })
     .catch(function (error) {
